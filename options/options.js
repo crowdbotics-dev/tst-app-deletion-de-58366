15d96de4eb13d12c8fc4f4fb34ad3ff5b121aef1
Crowdbotics <engineering@crowdbotics.com>
2023-01-20 16:33:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tst-app-deletion-de-58366",
+  url: "https://tst-app-deletion-de-58366.botics.co",
+  api: "https://tst-app-deletion-de-58366.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
